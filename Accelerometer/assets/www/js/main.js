da37aedb64df3b7acdcdc284e07549286dff6145
Eric Wilcox <ewilcox42@gmail.com>
2012-11-11 19:41:26 -0700

    Included Parse.com keys/libs and started accelerometer function

--- Accelerometer/assets/www/js/main.js
+++ Accelerometer/assets/www/js/main.js
@@ -1,5 +1,31 @@
+Parse.initialize("NEYolN8G2CaKtAPYcaeYX0nrSrP5tbhQ1bcwB16a", "N2kyDwAWWPx8Jg1azm8ifeJ6fYzF6DHpfmhVWQJc");
+var AccelObject = Parse.Object.extend("AccelObject");
+
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
+
+//navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+
 function onDeviceReady() {
-	alert("device ready!");
+	var data = new AccelObject();
+	//data.save( { test:"data1", test2:"data2" } );
+	
+	$("#startButton").click( function()
+        {
+			//alert("start button clicked");
+        });
+	$("#stopButton").click( function()
+        {
+			//alert("stop button clicked");
+        });
 }
 
 function init() {
